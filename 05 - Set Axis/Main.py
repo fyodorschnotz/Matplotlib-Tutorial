ae66666dcf4f9dc9b2b6da2c38004c9357cad10e
fyodorschnotz <fyodorschnotz@gmail.com>
2020-08-21 21:39:27 +0700

    cara mensetting axis

---
+++ 05 - Set Axis/Main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#membuat data
+def sinusGenerator(amplitudo,frekuensi,tAkhir,theta):
+    t = np.arange(0,tAkhir,0.1)
+    y = amplitudo * np.sin(2*frekuensi*t + np.deg2rad(theta))
+    return t,y
+
+t,y = sinusGenerator(1,1,4,0)
+
+#membuat plot
+plt.plot(t,y)
+
+#setting axis, minimu dan maximum
+plt.axis([0,4,-2,2])
+
+#menampilkan
+plt.show()
